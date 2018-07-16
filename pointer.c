d9ccde1dbd89c4eef35a3d0aa06719325b8cce21
urakamikit <urakami.naoya149@mail.kyutech.jp>
2018-07-16 21:04:31 +0900

    error:argument type 'void' is incomplete..

--- pointer.c
+++ pointer.c
@@ -8,7 +8,17 @@ Define pick up pointer to void for %p. So, should cast with void*.
 
 #include <stdio.h>
 
+void test_1(void);
+void test_2(void);
+
 int main(void) {
+  test_1();
+  test_2();
+  return 0;
+  }
+
+void test_1(void) {
+  
   int hoge = 5, piyo = 10;
   int* hoge_pointer;
 
@@ -27,6 +37,15 @@ int main(void) {
   //Change value of hoge through hoge_pointer
   *hoge_pointer = 10;
   printf("hoge..%d\n", hoge);
+} 
 
-  return 0;
-  }
+void test_2(void) {
+  int hoge = 5;
+  void* hoge_pointer;
+
+  hoge_pointer = &hoge;
+  
+  //Print value of hoge_pointer through hoge
+  //printf("%d\n" , *hoge_pointer);
+}
+
