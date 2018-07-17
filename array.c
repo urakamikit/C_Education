1d9f74ce8f3f0e2024242dbe1e2fb4d2dbcfbc1b
urakamikit <urakami.naoya149@mail.kyutech.jp>
2018-07-17 15:45:15 +0900

    Remade error

--- array.c
+++ array.c
@@ -6,9 +6,11 @@ Anyway, will use array.
 #include <stdio.h>
 
 void test_1(void);
+void test_2(void);
 
 int main(void) {
-  test_1();
+ // test_1();
+ test_2();
   return 0;
 }
 
@@ -28,5 +30,20 @@ void test_1(void) {
   //Print address of each element in array
   for(int i = 0 ; i < 5 ; i++) {
     printf("&array[%d]...%p\n" , i , (void*)&array[i]);
+  }
+}
+
+void test_2(void) {
+  int array[5];
+  int* pointer;
+
+  //Set value to each array
+  for (int i = 0 ; i < 5 ; i++) {
+    array[i] = i;
+  }
+
+  //Print value
+  for (pointer = &array[0]; pointer != &array[5]; pointer++) {
+    printf("%d\n",*pointer);
   }
 }
