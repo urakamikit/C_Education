06641cb868e9e86c730e5ec6bcb27bddc7fa8324
urakamikit <urakami.naoya149@mail.kyutech.jp>
2018-08-24 00:22:58 +0900

    There is a bug in this function. Need some array bytes to hold shifed data array. But not enough data memory in my condition with PIC. So, it is necessary to think another way.

--- shift_func.c
+++ shift_func.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint8_t array_1[20] = {0};
-uint8_t array_2[4];
+uint8_t array_1[43] = {0};
+uint8_t array_2[6];
 
 void insert_array(uint8_t*,uint8_t*,uint8_t);
+void insert(uint8_t* ,uint8_t* ,int);
 
 int main(void) {
-  for(int i = 0 ; i < 10 ; i++) {
+  for(int i = 0 ; i < 36 ; i++) {
     array_1[i] = i;
     printf("array_1[%d]:%d\n",i,array_1[i]);
   }
 
-  for(int j = 0 ; j < 4 ; j++) {
+  for(int j = 0 ; j < 6 ; j++) {
     array_2[j] = (j + 1)*10;
     printf("array_2[%d]:%d\n",j,array_2[j]);
   }
 
-  insert_array(&array_1[5],&array_2[0],4);
-  
-  for(int i = 0 ; i < 20 ; i++) {
+ // insert_array(&array_1[5],&array_2[0],4);
+  insert(&array_1[20],&array_1[23],3); 
+  for(int i = 0 ; i < 35 ; i++) {
       printf("array_1[%d]:%d\n",i,array_1[i]);
     }
 
-   for(int j = 0 ; j < 4 ; j++) {
-     printf("array_2[%d]:%d\n",j,array_2[j]);
+ // insert(&array_1[20],&array_1[23],3);
     }
 
-    }
-
-void insert_array(uint8_t* original_data, uint8_t* insert_data, uint8_t noBytes) {
-  for(int i = 0 ; i < noBytes ; i++) {
+/*void insert_array(uint8_t* original_data, uint8_t* insert_data, uint8_t noBytes, uint8_t max_bytes) {
+  for(int i = 0 ; i < noBytes - 1 ; i++) {
    uint8_t coppy_data = *(original_data + i);
    *(original_data + i) = *(insert_data + i);
-   *(original_data + noBytes + i) = coppy_data;
   }
-}
+}*/
+
+  void insert(uint8_t *start, uint8_t *last, int val) {
+    for(uint8_t *dst = last - 1; dst-- >= start; ) {
+       *(dst + 1) = *dst;
+       *start = val;
+    }
+  }
