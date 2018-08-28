24cc2e22bd7a011e74279415e1bb46b866f19f47
urakamikit <urakami.naoya149@mail.kyutech.jp>
2018-08-28 22:47:19 +0900

    Could take one byte by one byte from text file.

---
+++ file_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  FILE* file_ptr;
+
+  /*Pick up file with file pointer.*/
+  if((file_ptr = fopen("hoge.txt","r")) == NULL) {
+    printf("file open error!\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  char data;
+
+  while((data = getc(file_ptr)) != EOF) {
+    printf("%c\n",data);
+  }
+
+  return 0;
+}
